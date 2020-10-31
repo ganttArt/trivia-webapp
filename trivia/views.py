85d82a7570bafa5e84bebf4a32d261b44ff0ba8d
Chris Gantt <gantt.art@gmail.com>
2020-10-31 15:04:29 -0700

    Added a question number to each question object after they have been randomly sorted.

--- trivia/views.py
+++ trivia/views.py
@@ -5,13 +5,17 @@ from django.views.generic import TemplateView
 
 
 class Question:
-    def __init__(self, question, answers, correct_answer):
+    def __init__(self, question, answers, correct_answer, number=0):
         self.question = question
         self.answers = answers
         self.correct_answer = correct_answer
+        self.number = number
 
         self.answers.append(correct_answer)
         random.shuffle(self.answers)
+    
+    def assign_number(self, number):
+        self.number = number
 
 
 class QuestionsView(TemplateView):
@@ -32,6 +36,13 @@ class QuestionsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         random.shuffle(self.questions)
+
+        num = 1
+        for question in self.questions:
+            question.assign_number(str(num))
+            print(question.number)
+            num += 1
+        
         self.questions = self.questions[:20]
         context['questions'] = self.questions
         return context
